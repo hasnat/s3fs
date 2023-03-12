d6aee192b64a030f5652ae5c500be01bb2581b0b
Hasnat Ullah <hasnat.ullah@gmail.com>
2023-03-12 12:23:31 +0000

    echo

--- regression-run/README.md
+++ regression-run/README.md
@@ -6,7 +6,8 @@ test.sh waits on tests exit code.
 ## Run tests
 ```
 docker-compose -f regression-run/docker-compose.yml up -d fake-s3
-docker-compose -f regression-run/docker-compose.yml up s3fs-tests
+docker-compose -f regression-run/docker-compose.yml up --build s3fs-tests --exit-code-from s3fs-tests
+docker-compose -f regression-run/docker-compose.yml up --build s3fs-test-multiple-versions --exit-code-from s3fs-test-multiple-versions
 ```
 
 ## Run multiple versions test
--- regression-run/test-multiple-versions/entrypoint.sh
+++ regression-run/test-multiple-versions/entrypoint.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -euxo pipefail
-NODEJS_VERSIONS_TO_TEST="19.7.0 18.15.0 17.9.1 16.19.1 15.14.0 14.21.3 13.14.0 12.22.12 11.15.0 10.24.1 9.11.2 8.17.0 7.10.1 6.17.1 5.12.0 4.9.1 0.12.18"
-COMPOSE_PROJECT_NAME=${1:-s3fs-test}
-export COMPOSE_PROJECT_NAME
 
 function cleanup()
 {
@@ -16,5 +13,9 @@ trap cleanup EXIT
 for NODEJS_VERSION in $NODEJS_VERSIONS_TO_TEST
 do
     export NODEJS_VERSION="${NODEJS_VERSION}"
-    docker-compose -f regression-run/docker-compose.yml up --build --exit-code-from s3fs-tests
+    echo "======================Start testing nodejs version ${NODEJS_VERSION}============================="
+    echo ""
+    docker-compose -f regression-run/docker-compose.yml up --build s3fs-tests --exit-code-from s3fs-tests
+    echo ""
+    echo "======================Done testing nodejs version ${NODEJS_VERSION}=============================="
 done
